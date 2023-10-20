0cc1aab7d4b5cbce8c8c45758f284bd437f75797
Jubilee Young <workingjubilee@gmail.com>
2023-10-20 15:22:02 -0700

    Refactor MemCx and add sparse docs

--- pgrx/src/mem.rs
+++ pgrx/src/mem.rs
@@ -1,3 +1,4 @@
+#![deny(unsafe_op_in_unsafe_fn)]
 use crate::pg_sys;
 use core::{marker::PhantomData, ptr::NonNull};
 
@@ -8,22 +9,36 @@ pub struct MemCx<'mcx> {
 }
 
 impl<'mcx> MemCx<'mcx> {
-    /// You probably shouldn't be using this.
-    pub(crate) unsafe fn from_ptr(raw: pg_sys::MemoryContext) -> MemCx<'mcx> {
-        let ptr = NonNull::new(raw).expect("memory context must be non-null");
-        MemCx { ptr, _marker: PhantomData }
+    /// Create a MemCx from a [pg_sys::MemoryContext].
+    ///
+    /// # Safety
+    ///
+    /// Dereferences the pointer and generates a memory context with the inferred lifetime.
+    /// This should be made public only after we have other safe interfaces published first,
+    /// as it is both easy and also pretty incorrect.
+    pub(crate) unsafe fn from_ptr(ptr: pg_sys::MemoryContext) -> Option<MemCx<'mcx>> {
+        unsafe {
+            pg_sys::MemoryContextIsValid(ptr)
+                .then_some(MemCx { ptr: NonNull::new_unchecked(ptr), _marker: PhantomData })
+        }
     }
 
-    /// You probably shouldn't be using this.
+    /// Forwards to [pg_sys::MemoryContextAlloc].
     pub(crate) unsafe fn alloc_bytes(&self, size: usize) -> *mut u8 {
-        pg_sys::MemoryContextAlloc(self.ptr.as_ptr(), size).cast()
+        // SAFETY: We have no real safety invariants to uphold, aside from those that pgrx enforces.
+        unsafe { pg_sys::MemoryContextAlloc(self.ptr.as_ptr(), size).cast() }
     }
 
-    /// You probably shouldn't be using this.
+    /// Run some code inside this context, switching to it and then switching back.
     pub(crate) unsafe fn exec_in<T>(&self, f: impl FnOnce() -> T) -> T {
-        let remembered = pg_sys::MemoryContextSwitchTo(self.ptr.as_ptr());
-        let res = f();
-        pg_sys::MemoryContextSwitchTo(remembered);
+        let res;
+        // SAFETY: This sort of thing is why we need only one thread to be manipulating the Postgres context.
+        // Fortunately, pgrx runs main thread checks and the like whenever we call Postgres functions.
+        unsafe {
+            let remembered = pg_sys::MemoryContextSwitchTo(self.ptr.as_ptr());
+            res = f();
+            pg_sys::MemoryContextSwitchTo(remembered);
+        }
         res
     }
 }
@@ -33,7 +48,8 @@ pub fn current_context<'curr, F, T>(f: F) -> T
 where
     F: for<'clos> FnOnce(&'clos MemCx<'curr>) -> T,
 {
-    let memcx = unsafe { MemCx::from_ptr(pg_sys::CurrentMemoryContext) };
+    let memcx = unsafe { MemCx::from_ptr(pg_sys::CurrentMemoryContext) }
+        .expect("no current memory context?");
     let ret = { f(&memcx) };
     ret
 }
